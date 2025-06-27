147a96c5dc21b4982475c4dac1f09c6ecc3168a3
Patthh <luffinza@gmail.com>
2025-06-27 18:12:15 +0700

    enhance readability on 01

---
+++ read/ATarget.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+typedef std::string str;
+using	std::cout;
+using	std::endl;
+
+class ASpell;
+
+class ATarget{
+	protected:
+		str _target;
+	private:
+		ATarget(const str &target);
+		virtual ~ATarget();
+		const str &getType() const;
+		virtual ATarget* clone() const = 0;
+		void getHitBySpell(const ASpell &spell) const;
+};
---
+++ read/Warlock.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+
+typedef std::string str;
+using std::cout;
+using std::endl;
+using std::map;
+
+class ASpell;
+class ATarget;
+
+class Warlock{
+	private:
+		str _name, _title;
+		map<str, ASpell*> _sB;
+	public:
+		Warlock();
+		~Warlock();
+		const str &getName() const;
+		const str &getTitle() const;
+		void setTitle(const str &title);
+		void introduce() const;
+		void learnSpell(ASpell *spell);
+		void forgetSpell(str spellName);
+		void launchSpell(str spellName, const ATarget &target)
+};
