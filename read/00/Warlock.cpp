693cce62e8c4089804e64b94ce7a96e354325ac0
Patthh <luffinza@gmail.com>
2025-06-29 10:55:39 +0700

    em main on read00

--- read/00/Warlock.cpp
+++ read/00/Warlock.cpp
@@ -18,3 +18,21 @@ Warlock::Warlock(const s &n, const s &t): _n(n), _t(t){
 Warlock::~Warlock(){
 	cout << getName() << ": My job here is done!" << endl;
 }
+
+// #include "Warlock.hpp"
+
+// int main()
+// {
+//   Warlock const richard("Richard", "Mistress of Magma");
+//   richard.introduce();
+//   std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+
+//   Warlock* jack = new Warlock("Jack", "the Long");
+//   jack->introduce();
+//   jack->setTitle("the Mighty");
+//   jack->introduce();
+
+//   delete jack;
+
+//   return (0);
+// }
